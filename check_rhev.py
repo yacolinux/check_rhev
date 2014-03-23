74d80adcc68f91e4d8f7f7f5c4ae73635899310a
Marius Pana <mp@sphs.ro>
2014-03-23 09:23:30 +0200

    working on going from perl to python looking at requests and lxml since rhev api uses

--- check_rhev.py
+++ check_rhev.py
@@ -1,15 +1,31 @@
 #/usr/bin/env python
 # 
 # spearhead systems srl 2014
-# based on check_rhev3.pl from https://github.com/ovido/check_rhev3/
-#
+# based on check_rhev3.pl 
+# https://github.com/ovido/check_rhev3/
+# https://github.com/spearheadsys/check_rhev
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
+parser.add_argument("-v", "--versbose", help="Increase output verbosity", action="store_true")
+parser.add_argument("-V", "--version", help="Print version information")
+parser.add_argument("-H", "--host", help="The RHEVM/Ovirt host to query")
+parser.add_argument("-p", "--port", help="The RHEVm/Ovir port", type=int)
+parser.add_argument("-a", "--authorization", help="Username\@domain:password pair required for lgin to the REST API")
 parser.parse_args()
-print args.echo
 
 # rhev / ovirt defaults
 rhevm_port  = 443          # default port
@@ -19,7 +35,7 @@ rhevm_user = None          # default username
 rhevm_pass = None          # default password
 
 # Variables
-prog      = "check_rhev3"
+prog       = "check_rhev"
 version    = "0.0"
 projecturl = "https://github.com/spearheadsys/check_rhev"
 cookie     = "/tmp"   # default path to cookie file
@@ -54,21 +70,29 @@ try:
 except NameError:
   print "RHEV Manager hostname is missing.\n"
   print_usage()
-else:
-  print "sure, it was defined."
 
 
 ### perly
- if (! defined( $o_rhevm_host )){
-    print "RHEV Manager hostname is missing.\n";
-    print_usage();
-    exit $ERRORS{$status{'unknown'}};
-  }
+# if (! defined( $o_rhevm_host )){
+#    print "RHEV Manager hostname is missing.\n";
+#    print_usage();
+#    exit $ERRORS{$status{'unknown'}};
+#  }
+##
+
+
+def print_usage():
+    print "Usage:"
 
 ## perly
- sub print_usage(){
-  print "Usage: $0 [-v] -H <hostname> [-p <port>] -a <auth> | -f <authfile> [--ca-file <ca-file> [-o ] \n";
-  print "       [-A <api>] [-t <timeout>] -D <data center> | -C <cluster> | -R <rhev host> [-n <nic>] \n";
-  print "       | -S <storage domain> | -M <vm> | -P <vmpool> [-w <warn>] [-c <critical>] [-V] \n";
-  print "       [-l <check>] [-s <subcheck>]\n"; 
-}
+# sub print_usage(){
+#  print "Usage: $0 [-v] -H <hostname> [-p <port>] -a <auth> | -f <authfile> [--ca-file <ca-file> [-o ] \n";
+#  print "       [-A <api>] [-t <timeout>] -D <data center> | -C <cluster> | -R <rhev host> [-n <nic>] \n";
+#  print "       | -S <storage domain> | -M <vm> | -P <vmpool> [-w <warn>] [-c <critical>] [-V] \n";
+#  print "       [-l <check>] [-s <subcheck>]\n"; 
+#}
+
+# at one point, when we actually are doing something
+# do something!
+if args.verbosity:
+    print "verbosity turned on"
